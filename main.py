ea7c6ee4bfbbc6e68d2db0bab06a70cd0c8d5c67
w50111831 <danielhitchcock236@proton.me>
2025-10-20 23:12:41 +0100

    Add pages and switch to grid layout

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Main():
         self.window.page.destroy()
         self.window.page = page
         self.window.title(page.title)
-        page.pack(fill="both", expand=True)
+        self.window.geometry(f"{page.WIDTH}x{page.HEIGHT}")
 
 
 #Window wrapper for tkinter canvas.
@@ -22,6 +22,8 @@ class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.page = tk.Frame(self)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
 
 #Base page class as a framework for future Pages:
@@ -29,30 +31,46 @@ class BasePage(tk.Frame):
     def __init__(self, window, main):
         super().__init__(window)
         self.main = main
+        self.grid(column=0, row=0, sticky="nsew")
+        for i in range(3):
+            self.grid_rowconfigure(i, weight=1)
+            self.grid_columnconfigure(i, weight=1) 
+            
     title = "Default title"
+    WIDTH = 900
+    HEIGHT = 900
 
 #All pages:
 class HomePage(BasePage):
     def __init__(self, window, main):
         super().__init__(window, main)
-        ttk.Label(self, text="Homepage").pack(expand=True)
-        ttk.Button(self, text="Demand curve", command=lambda: main.showpage(DPage(window, main))).pack()
-        ttk.Button(self, text="Supply curve", command=lambda: main.showpage(SPage(window, main))).pack()
+        self.grid(column=0, row=0, sticky="nsew")
+        self.grid_rowconfigure(0, weight=3)
+        for i in range(1,12):
+            self.grid_rowconfigure(i, weight=2)
+        for i in range(3):
+            self.grid_columnconfigure(i, weight=1)
+        ttk.Label(self, text="Homepage").grid(column=1, row=0, sticky="nsew")
+        ttk.Button(self, text="Demand curve", command=lambda: main.showpage(DPage(window, main))).grid(column=0, row=1, sticky="nsew")
+        ttk.Button(self, text="Supply curve", command=lambda: main.showpage(SPage(window, main))).grid(column=0, row=2, sticky="nsew")
     title = "Homepage"
 
 class DPage(BasePage):
     def __init__(self, window, main):
         super().__init__(window, main)
-        ttk.Label(self, text="Demand curve generator").pack(expand=True)
-        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).pack()
+        ttk.Label(self, text="Demand curve generator").grid(column=1, row=0, sticky="nsew")
+        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).grid(column=0, row=1, sticky="nsew")
+        
+
     title = "Demand curve generator"
 
 class SPage(BasePage):
     def __init__(self, window, main):
         super().__init__(window, main)
-        ttk.Label(self, text="Supply curve generator").pack(expand=True)
-        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).pack()
+        ttk.Label(self, text="Supply curve generator").grid(column=1, row=0, sticky="nsew")
+        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).grid(column=0, row=1, sticky="nsew")
     title = "Supply curve generator"
+
 
 main = Main() #instantiate main object
 
