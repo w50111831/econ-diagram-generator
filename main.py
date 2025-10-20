ca69d9391cdb42eca33ec13e0df41989aa9c3dc8
w50111831 <danielhitchcock236@proton.me>
2025-10-20 21:02:17 +0100

    Make object orientated page navigation and creatio n

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import tkinter as tk
 from tkinter import ttk
+#diag_list = ['PPC','PPF', 'D', 'S', 'DS','SURPLUS', 'SHORTAGE', 'PED', 'XED']
+class Main():
+    def __init__(self):
+        self.window = Window() #instantiate window in main
+        page = HomePage(self.window, self) 
+        self.showpage(page) #set page to homepage, then show the new page value.
+        self.window.mainloop()
 
-root = tk.Tk()
+    def showpage(self, page):
+        self.window.page.destroy()
+        self.window.page = page
+        self.window.title(page.title)
+        page.pack(fill="both", expand=True)
 
-diag_list = ['PPC','PPF', 'D', 'S', 'DS','SURPLUS', 'SHORTAGE', 'PED', 'XED']
 
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.page = tk.Frame(self)
 
+class HomePage(tk.Frame):
+    def __init__(self, window, main):
+        super().__init__(window)
+        ttk.Label(self, text="Homepage").pack(expand=True,)
+        ttk.Button(self, text="Demand curve").pack()
+        self.title = "Homepage"
 
+main = Main() #instantiate main object
 
-pagetitle = ttk.Label(root)
-pagetitle['text'] = 'Economics diagram generator'
-pagetitle.pack()
-
-root.mainloop()
+        
