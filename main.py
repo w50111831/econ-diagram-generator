357cb5d7bc9247f4bfcee30a3b71a3a2891136f1
w50111831 <danielhitchcock236@proton.me>
2025-10-20 21:58:27 +0100

    Add base page class

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 #diag_list = ['PPC','PPF', 'D', 'S', 'DS','SURPLUS', 'SHORTAGE', 'PED', 'XED']
+
+#Main class
 class Main():
     def __init__(self):
         self.window = Window() #instantiate window in main
@@ -15,17 +17,42 @@ class Main():
         page.pack(fill="both", expand=True)
 
 
+#Window wrapper for tkinter canvas.
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.page = tk.Frame(self)
 
-class HomePage(tk.Frame):
+
+#Base page class as a framework for future Pages:
+class BasePage(tk.Frame):
     def __init__(self, window, main):
         super().__init__(window)
-        ttk.Label(self, text="Homepage").pack(expand=True,)
-        ttk.Button(self, text="Demand curve").pack()
-        self.title = "Homepage"
+        self.main = main
+    title = "Default title"
+
+#All pages:
+class HomePage(BasePage):
+    def __init__(self, window, main):
+        super().__init__(window, main)
+        ttk.Label(self, text="Homepage").pack(expand=True)
+        ttk.Button(self, text="Demand curve", command=lambda: main.showpage(DPage(window, main))).pack()
+        ttk.Button(self, text="Supply curve", command=lambda: main.showpage(SPage(window, main))).pack()
+    title = "Homepage"
+
+class DPage(BasePage):
+    def __init__(self, window, main):
+        super().__init__(window, main)
+        ttk.Label(self, text="Demand curve generator").pack(expand=True)
+        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).pack()
+    title = "Demand curve generator"
+
+class SPage(BasePage):
+    def __init__(self, window, main):
+        super().__init__(window, main)
+        ttk.Label(self, text="Supply curve generator").pack(expand=True)
+        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).pack()
+    title = "Supply curve generator"
 
 main = Main() #instantiate main object
 
