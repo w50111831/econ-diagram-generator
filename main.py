faecfca53df26cd2201f40a5a3bf5820f51a0d80
w50111831 <danielhitchcock236@proton.me>
2025-10-21 13:37:30 +0100

    Create flexbox like system with strict grid boxes that have expanding widgets inside.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tkinter import ttk
 class Main():
     def __init__(self):
         self.window = Window() #instantiate window in main
+        self.createstyles()
         page = HomePage(self.window, self) 
         self.showpage(page) #set page to homepage, then show the new page value.
         self.window.mainloop()
@@ -15,6 +16,10 @@ class Main():
         self.window.page = page
         self.window.title(page.title)
         self.window.geometry(f"{page.WIDTH}x{page.HEIGHT}")
+    
+    def createstyles(self):
+        titlestyle= ttk.Style()
+        titlestyle.configure('Title.TButton', font=('helvetica', 20), padding=0)
 
 
 #Window wrapper for tkinter canvas.
@@ -26,16 +31,26 @@ class Window(tk.Tk):
         self.grid_columnconfigure(0, weight=1)
 
 
+
 #Base page class as a framework for future Pages:
 class BasePage(tk.Frame):
     def __init__(self, window, main):
         super().__init__(window)
         self.main = main
         self.grid(column=0, row=0, sticky="nsew")
-        for i in range(3):
-            self.grid_rowconfigure(i, weight=1)
-            self.grid_columnconfigure(i, weight=1) 
-            
+        for i in range(15):
+            self.grid_rowconfigure(i, weight=1, uniform="grid")
+            self.grid_columnconfigure(i, weight=1, uniform="grid")
+        #self.grid_propagate(False)
+    
+    def strictboxwidget(self, column=0, row=0, columnspan=1, rowspan=1):
+        frame = tk.Frame(self)
+        frame.grid(column=column, row=row, columnspan=columnspan, sticky="nsew")
+        frame.grid_propagate(False)
+        return frame
+
+
+
     title = "Default title"
     WIDTH = 900
     HEIGHT = 900
@@ -44,22 +59,24 @@ class BasePage(tk.Frame):
 class HomePage(BasePage):
     def __init__(self, window, main):
         super().__init__(window, main)
-        self.grid(column=0, row=0, sticky="nsew")
-        self.grid_rowconfigure(0, weight=3)
-        for i in range(1,12):
-            self.grid_rowconfigure(i, weight=2)
-        for i in range(3):
-            self.grid_columnconfigure(i, weight=1)
-        ttk.Label(self, text="Homepage").grid(column=1, row=0, sticky="nsew")
-        ttk.Button(self, text="Demand curve", command=lambda: main.showpage(DPage(window, main))).grid(column=0, row=1, sticky="nsew")
-        ttk.Button(self, text="Supply curve", command=lambda: main.showpage(SPage(window, main))).grid(column=0, row=2, sticky="nsew")
+
+        ttk.Label(self.strictboxwidget(row=1, column=4, columnspan=6), text="Homepage", style="Title.TButton", anchor="center" ).place(relx=0, rely=0, relwidth=1, relheight=1)
+
+        ttk.Button(self, text="Demand curve", command=lambda: main.showpage(DPage(window, main))).grid(column=2, row=4, columnspan=2, sticky="nsew")
+        ttk.Button(self, text="Supply curve", command=lambda: main.showpage(SPage(window, main))).grid(column=2, row=6, columnspan=2, stick="nsew")
     title = "Homepage"
 
 class DPage(BasePage):
     def __init__(self, window, main):
         super().__init__(window, main)
-        ttk.Label(self, text="Demand curve generator").grid(column=1, row=0, sticky="nsew")
-        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).grid(column=0, row=1, sticky="nsew")
+
+        frame = tk.Frame(self)
+        frame.grid(column=4, row=1, columnspan=6, rowspan=1, sticky="nsew")
+        frame.grid_propagate(False)
+        ttk.Label(frame, text="Demand curve generator", style="Title.TButton", anchor="center").place(relx=0, rely=0, relwidth=1, relheight=1)
+        
+        
+        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).grid(column=1, row=4, columnspan=2, sticky="nsew")
         
 
     title = "Demand curve generator"
@@ -67,8 +84,8 @@ class DPage(BasePage):
 class SPage(BasePage):
     def __init__(self, window, main):
         super().__init__(window, main)
-        ttk.Label(self, text="Supply curve generator").grid(column=1, row=0, sticky="nsew")
-        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).grid(column=0, row=1, sticky="nsew")
+        ttk.Label(self, text="Supply curve generator", style="Title.TButton", anchor="center").grid(column=4, row=1, columnspan=6, rowspan=1, sticky="nsew")
+        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).grid(column=1, row=4, columnspan=2, sticky="nsew")
     title = "Supply curve generator"
 
 
