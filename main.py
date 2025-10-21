542d04c468b4ef7e08da3ddae660e2312616a7b1
w50111831 <danielhitchcock236@proton.me>
2025-10-21 14:57:12 +0100

    Add custom line drawer that takes perentages, also add canvas to demand page

--- main.py
+++ main.py
@@ -45,10 +45,19 @@ class BasePage(tk.Frame):
     
     def strictboxwidget(self, column=0, row=0, columnspan=1, rowspan=1):
         frame = tk.Frame(self)
-        frame.grid(column=column, row=row, columnspan=columnspan, sticky="nsew")
+        frame.grid(column=column, row=row, columnspan=columnspan, rowspan=rowspan, sticky="nsew")
         frame.grid_propagate(False)
         return frame
-
+    
+    def line_coords(self, x1, y1, x2, y2, canvas):
+        canvas_width = canvas.winfo_width()
+        canvas_height = canvas.winfo_height()
+        x1 = x1*canvas_width
+        x2 = x2*canvas_width
+        y1 = y1*canvas_height
+        y2 = y2*canvas_height
+        coords = [x1,y1,x2,y2]
+        return canvas.create_line(coords, fill='black', width=2)
 
 
     title = "Default title"
@@ -61,31 +70,34 @@ class HomePage(BasePage):
         super().__init__(window, main)
 
         ttk.Label(self.strictboxwidget(row=1, column=4, columnspan=6), text="Homepage", style="Title.TButton", anchor="center" ).place(relx=0, rely=0, relwidth=1, relheight=1)
+        ttk.Button(self.strictboxwidget(column=2, row=4, columnspan=2 ), text="Demand curve", command=lambda: main.showpage(DPage(window, main))).place(relx=0, rely=0, relwidth=1, relheight=1)
+        ttk.Button(self.strictboxwidget(column=2, row=6, columnspan=2 ), text="Supply curve", command=lambda: main.showpage(SPage(window, main))).place(relx=0, rely=0, relwidth=1, relheight=1)
 
-        ttk.Button(self, text="Demand curve", command=lambda: main.showpage(DPage(window, main))).grid(column=2, row=4, columnspan=2, sticky="nsew")
-        ttk.Button(self, text="Supply curve", command=lambda: main.showpage(SPage(window, main))).grid(column=2, row=6, columnspan=2, stick="nsew")
     title = "Homepage"
 
 class DPage(BasePage):
     def __init__(self, window, main):
         super().__init__(window, main)
 
-        frame = tk.Frame(self)
-        frame.grid(column=4, row=1, columnspan=6, rowspan=1, sticky="nsew")
-        frame.grid_propagate(False)
-        ttk.Label(frame, text="Demand curve generator", style="Title.TButton", anchor="center").place(relx=0, rely=0, relwidth=1, relheight=1)
-        
-        
-        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).grid(column=1, row=4, columnspan=2, sticky="nsew")
-        
+        ttk.Label(self.strictboxwidget(row=1, column=4, columnspan=6), text="Demand curve generator", style="Title.TButton", anchor="center" ).place(relx=0, rely=0, relwidth=1, relheight=1)
+        ttk.Button(self.strictboxwidget(row=4, column=1, columnspan=2), text="HomePage", command=lambda: main.showpage(HomePage(window, main))).place(relx=0, rely=0, relwidth=1, relheight=1)
 
+        diagram = tk.Canvas(self.strictboxwidget(row=5, column=6, columnspan=8, rowspan=8), width=100, height=100, background='gray75')
+        diagram.place(relx=0, rely=0, relwidth=1, relheight=1)
+        diagram.update()
+        self.line_coords(0.1, 0.9, 0.9, 0.9, diagram)
+        self.line_coords(0.1, 0.9, 0.1, 0.1, diagram)
+
+        
     title = "Demand curve generator"
 
 class SPage(BasePage):
     def __init__(self, window, main):
         super().__init__(window, main)
-        ttk.Label(self, text="Supply curve generator", style="Title.TButton", anchor="center").grid(column=4, row=1, columnspan=6, rowspan=1, sticky="nsew")
-        ttk.Button(self, text="HomePage", command=lambda: main.showpage(HomePage(window, main))).grid(column=1, row=4, columnspan=2, sticky="nsew")
+
+        ttk.Label(self.strictboxwidget(row=1, column=4, columnspan=6), text="Supply curve generator", style="Title.TButton", anchor="center").place(relx=0, rely=0, relwidth=1, relheight=1)
+        ttk.Button(self.strictboxwidget(row=4, columnspan=2), text="HomePage", command=lambda: main.showpage(HomePage(window, main))).place(relx=0, rely=0, relwidth=1, relheight=1)
+
     title = "Supply curve generator"
 
 
