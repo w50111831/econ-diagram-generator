a9046895e1b1b4d37cb9bd4c8f62c2d3e27ae6f1
w50111831 <danielhitchcock236@proton.me>
2025-10-22 17:01:42 +0100

    Add diagram functionality to DS

--- main.py
+++ main.py
@@ -113,19 +113,19 @@ class DPage(BasePage):
         
         
         #diagram.create_text(self.text_coords(0.5, 0.5, diagram), text="D", font=("helvetica", 16), fill="black")
-        quantityscale = ttk.Scale(self.strictboxwidget(row=13, column=7, columnspan=5), orient='horizontal', length = 100, from_=20, to=75, command=lambda q:self.updateequilibrium(q, diagram))
-        quantityscale.place(relx=0, rely=0, relwidth=1, relheight=1)
+        Doffsetscale = ttk.Scale(self.strictboxwidget(row=13, column=7, columnspan=5), orient='horizontal', length = 100, from_=20, to=75, command=lambda Doffset:self.updateequilibrium(Doffset, diagram))
+        Doffsetscale.place(relx=0, rely=0, relwidth=1, relheight=1)
         
 
         ttk.Label(self.strictboxwidget(row=6, column=12, columnspan=1), text="D", style="Diagram.TLabel", anchor="center" ).place(relx=0, rely=0, relwidth=1, relheight=1)
 
-    def updateequilibrium(self, q, diagram):
-        q=int(round(float(q)))
+    def updateequilibrium(self, Doffset, diagram):
+        Doffset=int(round(float(Doffset)))
         diagram.delete(self.UptoD, self.RighttoD)
-        self.UptoD = self.line_coords(q/100, 0.9, q/100, (1-(q/100)), diagram)
-        self.RighttoD = self.line_coords(0.1, (1-(q/100)), (q/100), (1-(q/100)), diagram)
-        self.currentquantity.config(text=f"quantity = {q}")
-        self.currentprice.config(text=f"price = {(100-q)}")
+        self.UptoD = self.line_coords(Doffset/100, 0.9, Doffset/100, (1-(Doffset/100)), diagram)
+        self.RighttoD = self.line_coords(0.1, (1-(Doffset/100)), (Doffset/100), (1-(Doffset/100)), diagram)
+        self.currentquantity.config(text=f"quantity = {Doffset}")
+        self.currentprice.config(text=f"price = {(100-Doffset)}")
 
 
 
@@ -159,28 +159,89 @@ class SPage(BasePage):
         
         
         #diagram.create_text(self.text_coords(0.5, 0.5, diagram), text="D", font=("helvetica", 16), fill="black")
-        quantityscale = ttk.Scale(self.strictboxwidget(row=13, column=7, columnspan=5), orient='horizontal', length = 100, from_=20, to=75, command=lambda q:self.updateequilibrium(q, diagram))
-        quantityscale.place(relx=0, rely=0, relwidth=1, relheight=1)
+        Doffsetscale = ttk.Scale(self.strictboxwidget(row=13, column=7, columnspan=5), orient='horizontal', length = 100, from_=20, to=75, command=lambda Doffset:self.updateequilibrium(Doffset, diagram))
+        Doffsetscale.place(relx=0, rely=0, relwidth=1, relheight=1)
         
 
         ttk.Label(self.strictboxwidget(row=10, column=12, columnspan=1), text="S", style="Diagram.TLabel", anchor="center" ).place(relx=0, rely=0, relwidth=1, relheight=1)
 
-    def updateequilibrium(self, q, diagram):
-        q=int(round(float(q)))
+    def updateequilibrium(self, Doffset, diagram):
+        Doffset=int(round(float(Doffset)))
         diagram.delete(self.UptoS, self.RighttoS)
-        self.UptoS = self.line_coords(q/100, 0.9, q/100, (q/100), diagram)
-        self.RighttoS = self.line_coords(0.1, (q/100), (q/100), (q/100), diagram)
-        self.currentquantity.config(text=f"quantity = {q}")
-        self.currentprice.config(text=f"price per unit = {q}")
+        self.UptoS = self.line_coords(Doffset/100, 0.9, Doffset/100, (Doffset/100), diagram)
+        self.RighttoS = self.line_coords(0.1, (Doffset/100), (Doffset/100), (Doffset/100), diagram)
+        self.currentquantity.config(text=f"quantity = {Doffset}")
+        self.currentprice.config(text=f"price per unit = {Doffset}")
 
 
 class DSPage(BasePage):
     def __init__(self, window, main):
         super().__init__(window, main)
 
-        ttk.Label(text="DS curve generator", style="Title.TLabel").grid(column=5, row=2)
+        ttk.Label(self.strictboxwidget(row=1, column=4, columnspan=6), text="D+S curve generator", style="Title.TLabel", anchor="center" ).place(relx=0, rely=0, relwidth=1, relheight=1)
+        ttk.Button(self.strictboxwidget(row=4, column=1, columnspan=2), text="HomePage", command=lambda: main.showpage(HomePage(window, main))).place(relx=0, rely=0, relwidth=1, relheight=1)
+
+        ttk.Label(self.strictboxwidget(row=13, column=12, columnspan=6), text="Quantity", anchor="center" ).place(relx=0, rely=0, relwidth=1, relheight=1)
+        self.currentquantity = ttk.Label(self.strictboxwidget(row=10, column=3, columnspan=2), text="Quantity is 50", anchor="center" )
+        self.currentquantity.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+        ttk.Label(self.strictboxwidget(row=5, column=4, columnspan=2), text="Price", anchor="center" ).place(relx=0, rely=0, relwidth=1, relheight=1)
+        self.currentprice = ttk.Label(self.strictboxwidget(row=10, column=1, columnspan=2), text="Price per unit is 50", anchor="center" )
+        self.currentprice.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+        #diagram area:
+        diagram = tk.Canvas(self.strictboxwidget(row=5, column=6, columnspan=8, rowspan=8), width=100, height=100, background='gray75')
+        diagram.place(relx=0, rely=0, relwidth=1, relheight=1)
+        diagram.update()
+        self.line_coords(0.1, 0.9, 0.9, 0.9, diagram) #x-axis line
+        self.line_coords(0.1, 0.9, 0.1, 0.1, diagram) #y-axis line
+        self.S = self.line_coords(0.2, 0.2, 0.8, 0.8, diagram)
+        self.D = self.line_coords(0.2, 0.8, 0.8, 0.2, diagram)
+        self.UptoE = self.line_coords(0.5, 0.9, 0.5, 0.5, diagram)
+        self.RighttoE = self.line_coords(0.1, 0.5, 0.5, 0.5, diagram)
+        
+        ttk.Label(self.strictboxwidget(row=10, column=12, columnspan=1), text="S", style="Diagram.TLabel", anchor="center" ).place(relx=0, rely=0, relwidth=1, relheight=1)
+
+        self.DLabelBackgroundFrame = self.strictboxwidget(row=5, column=8, columnspan=5)
+        self.DLabelBackground = ttk.Label(self.DLabelBackgroundFrame, text="", style="Diagram.TLabel", anchor="center" )
+        self.DLabelBackground.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+        self.DLabelFrame = self.strictboxwidget(row=5, column=12, columnspan=1)
+        self.DLabel = ttk.Label(self.DLabelFrame, text="D", style="Diagram.TLabel", anchor="center" )
+        self.DLabel.place(relx=0, rely=0, relwidth=1, relheight=1)
+        
+
+        #diagram.create_text(self.text_coords(0.5, 0.5, diagram), text="D", font=("helvetica", 16), fill="black")
+        Doffsetscale = ttk.Scale(self.strictboxwidget(row=13, column=7, columnspan=5), orient='horizontal', length = 100, from_=20, to=75, command=lambda Doffset:self.updateequilibrium(Doffset, diagram))
+        Doffsetscale.set(50)
+        Doffsetscale.place(relx=0, rely=0, relwidth=1, relheight=1)
+        
+
+        
+
+    def updateequilibrium(self, Doffset, diagram):
+        Doffset=int(round(float(Doffset)))
+
+        diagram.delete(self.D)
+        self.D = self.line_coords((-0.3+Doffset/100), 0.8, (0.3+Doffset/100), 0.2, diagram)
+        self.DLabel.destroy()
+        self.DLabelBackgroundFrame.destroy()
+        self.DLabelFrame.destroy()
+
+        self.DlabelBackgroundFrame = self.strictboxwidget(row=5, column=6, columnspan=8)
+        self.DLabelBackground = ttk.Label(self.DlabelBackgroundFrame, text="", style="Diagram.TLabel", anchor="center" )
+        self.DLabelBackground.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+        self.DLabelFrame = self.strictboxwidget(row=5, column=(round(7+Doffset/10)))
+        self.DLabel = ttk.Label(self.DLabelFrame, text="D", style="Diagram.TLabel", anchor="center" )
+        self.DLabel.place(relx=0, rely=0, relwidth=1, relheight=1)
+        
+        diagram.delete(self.UptoE, self.RighttoE)
+        self.UptoE = self.line_coords((0.5+(Doffset/100))/2, 0.9, (0.5+(Doffset/100))/2, (0.5+(Doffset/100))/2, diagram)
+        self.RighttoE = self.line_coords(0.1, (0.5+(Doffset/100))/2, (0.5+(Doffset/100))/2, (0.5+(Doffset/100))/2, diagram)
+        self.currentquantity.config(text=f"quantity = {Doffset}")
+        self.currentprice.config(text=f"price per unit = {Doffset}")
 
     title = "Supply curve generator"
-
 
 main = Main() #instantiate main object
